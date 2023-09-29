23fe5c90bef82423edee4c52628c64ad90ed67a0
Marco Ilari <marilari88@gmail.com>
2023-09-29 17:20:13 +0200

    ✨ Add login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from '@astrojs/tailwind'
 import {defineConfig} from 'astro/config'
-
+import nodejs from '@astrojs/node'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: nodejs({mode: 'standalone'}),
   integrations: [tailwind({applyBaseStyles: false}), react()],
 })
